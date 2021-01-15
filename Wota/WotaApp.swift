86e969079710ab1e6a5810cf304fc705d80c96f7
Camille Khubbetdinov <xdghost.kamil@gmail.com>
2021-01-15 21:09:07 +0300

    Initial Commit

---
+++ Wota/WotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WotaApp.swift
+//  Wota
+//
+//  Created by Camille Khubbetdinov on 15.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct WotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
